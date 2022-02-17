14e72f0351e56d56a92ef2e52a020fe95fb740e9
Yee Hong Low <yeehonglow@gmail.com>
2022-02-17 23:39:13 +0800

    fixed models typo

--- service/api/models.py
+++ service/api/models.py
@@ -50,7 +50,7 @@ class Track(models.Model):
         return "{}{}/{}".format(settings.DSP_BASE, self.id, "spotify")
 
 
-class Playlist(models.Models):
+class Playlist(models.Model):
     id = models.CharField(primary_key=True, max_length=10)
     name =  models.CharField(max_length=200, null=False)
     tracks = models.ManyToManyField(Track, related_name="playlist_tracks")
