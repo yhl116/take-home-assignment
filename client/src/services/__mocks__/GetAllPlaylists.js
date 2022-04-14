9780644ba9ff47a7cc644687a92147f0e1197890
Yee Hong Low <yeehonglow@gmail.com>
2022-04-14 20:55:25 +0800

    mocking get all playlists

---
+++ client/src/services/__mocks__/GetAllPlaylists.js
@@ -0,0 +1,119 @@
+export default {
+    async GetAllPlaylists() {
+        const playlists = [
+            {
+                "id": 2,
+                "name": "hello",
+                "tracks": [
+                    {
+                        "id": "0dQiJavYYb",
+                        "title": "Hellonelia",
+                        "length": 208,
+                        "bpm": 0,
+                        "genres": [
+                            "Indie Pop"
+                        ],
+                        "moods": [
+                            "Laid Back",
+                            "Sentimental"
+                        ],
+                        "main_artists": [
+                            "Martin Hall"
+                        ],
+                        "featured_artists": [],
+                        "audio": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.mp3",
+                        "cover_art": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.jpg",
+                        "waveform": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.json",
+                        "spotify": "http://link.epidemicsound.com/0dQiJavYYb/spotify"
+                    }
+                ]
+            },
+            {
+                "id": 16,
+                "name": "world",
+                "tracks": [
+                    {
+                        "id": "0dQiJavYYb",
+                        "title": "Hellonelia",
+                        "length": 208,
+                        "bpm": 0,
+                        "genres": [
+                            "Indie Pop"
+                        ],
+                        "moods": [
+                            "Laid Back",
+                            "Sentimental"
+                        ],
+                        "main_artists": [
+                            "Martin Hall"
+                        ],
+                        "featured_artists": [],
+                        "audio": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.mp3",
+                        "cover_art": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.jpg",
+                        "waveform": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.json",
+                        "spotify": "http://link.epidemicsound.com/0dQiJavYYb/spotify"
+                    },
+                    {
+                        "id": "iUFrtFTU1d",
+                        "title": "Evergreen Fields",
+                        "length": 194,
+                        "bpm": 118,
+                        "genres": [
+                            "Soft House"
+                        ],
+                        "moods": [
+                            "Happy",
+                            "Hopeful"
+                        ],
+                        "main_artists": [
+                            "Tape Machines"
+                        ],
+                        "featured_artists": [],
+                        "audio": "https://storage.googleapis.com/tech-coding-interview-assets/iUFrtFTU1d.mp3",
+                        "cover_art": "https://storage.googleapis.com/tech-coding-interview-assets/iUFrtFTU1d.jpg",
+                        "waveform": "https://storage.googleapis.com/tech-coding-interview-assets/iUFrtFTU1d.json",
+                        "spotify": "http://link.epidemicsound.com/iUFrtFTU1d/spotify"
+                    }
+                ]
+            },
+            {
+                "id": 26,
+                "name": "ccc",
+                "tracks": []
+            },
+            {
+                "id": 27,
+                "name": "aaa",
+                "tracks": []
+            },
+            {
+                "id": 4,
+                "name": "yeehong",
+                "tracks": [
+                    {
+                        "id": "0dQiJavYYb",
+                        "title": "Hellonelia",
+                        "length": 208,
+                        "bpm": 0,
+                        "genres": [
+                            "Indie Pop"
+                        ],
+                        "moods": [
+                            "Laid Back",
+                            "Sentimental"
+                        ],
+                        "main_artists": [
+                            "Martin Hall"
+                        ],
+                        "featured_artists": [],
+                        "audio": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.mp3",
+                        "cover_art": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.jpg",
+                        "waveform": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.json",
+                        "spotify": "http://link.epidemicsound.com/0dQiJavYYb/spotify"
+                    }
+                ]
+            }
+        ]
+        return playlists;
+    }
+}
