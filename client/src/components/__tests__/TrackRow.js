262ed9efc978ccf5bfb8c013c2a1bafbd192d7ca
Yee Hong Low <yeehonglow@gmail.com>
2022-04-14 21:52:48 +0800

    TrackRow snapshot test added params

--- client/src/components/__tests__/TrackRow.js
+++ client/src/components/__tests__/TrackRow.js
@@ -4,8 +4,32 @@ import renderer from "react-test-renderer";
 
 describe("The track row", () => {
     it("should match the snapshot", () => {
+        const track = {
+            "id": "0dQiJavYYb",
+            "title": "Hellonelia",
+            "length": 208,
+            "bpm": 0,
+            "genres": [
+                "Indie Pop"
+            ],
+            "moods": [
+                "Laid Back",
+                "Sentimental"
+            ],
+            "main_artists": [
+                "Martin Hall"
+            ],
+            "featured_artists": [],
+            "audio": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.mp3",
+            "cover_art": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.jpg",
+            "waveform": "https://storage.googleapis.com/tech-coding-interview-assets/0dQiJavYYb.json",
+            "spotify": "http://link.epidemicsound.com/0dQiJavYYb/spotify"
+        }
+
         const tree = renderer
-            .create(<TrackRow />)
+            .create(<TrackRow 
+            track = {track}
+            handlePlay={null}/>)
             .toJSON();
 
         expect(tree).toMatchSnapshot();
